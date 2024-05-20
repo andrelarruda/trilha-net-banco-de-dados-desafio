2a79152fd5e041ae71847c3db89cd79325f8f4eb
Andre Arruda <andrebass27@gmail.com>
2024-05-20 15:27:49 -0300

    Updating desafio.

--- Resposta Desafio.sql
+++ Resposta Desafio.sql
@@ -51,6 +51,42 @@ FROM Filmes
 WHERE Duracao>100 and Duracao<150
 ORDER BY Duracao
 
+-- 7 - Buscar a quantidade de filmes lançadas no ano, agrupando por ano, 
+-- ordenando pela duracao em ordem decrescente
+SELECT Ano, COUNT(Nome) AS 'Quantidade'
+FROM Filmes
+GROUP BY Ano
+ORDER BY SUM(Duracao) DESC
+
+-- 8 - Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero='M'
+
+-- 9 - Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero='F'
+ORDER BY PrimeiroNome
+
+-- 10 - Buscar o nome do filme e o gênero
+SELECT f.Nome AS 'Filme', g.Genero
+FROM Filmes f JOIN FilmesGenero fg ON f.Id=fg.IdFilme
+JOIN Generos g ON fg.IdGenero=g.Id
+
+-- 11 - Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT f.Nome AS 'Filme', g.Genero
+FROM Filmes f JOIN FilmesGenero fg ON f.Id=fg.IdFilme
+JOIN Generos g ON fg.IdGenero=g.Id
+WHERE Genero='Mistério'
+
+-- 12 - Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT f.Nome AS 'Filme', a.PrimeiroNome, a.UltimoNome, ef.Papel
+FROM Filmes f JOIN ElencoFilme ef ON f.Id=ef.IdFilme
+JOIN Atores a ON ef.IdAtor=a.Id
+
+
+
 
 
 
